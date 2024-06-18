e96693f3c84ed7fe9a020c787aa3bb1dc4846ebf
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-18 17:17:51 +0100

    add test to test proposal creation

---
+++ test/Voting.ts
@@ -0,0 +1,21 @@
+import hre from 'hardhat';
+import { expect } from 'chai';
+
+describe('Voting', function () {
+  async function deployContract() {
+    const Voting = await hre.ethers.getContractFactory('Voting');
+    const voting = await Voting.deploy();
+
+    return voting;
+  }
+
+  it('Should create a proposal', async function () {
+    const description = 'Proposal 1';
+    const voting = await deployContract();
+    await voting.createProposal(description);
+
+    const proposals = await voting.getProposals();
+    expect(proposals.length).to.equal(1);
+    expect(proposals[0].description).to.equal(description);
+    expect(proposals[0].voteCount).to.equal(0);
+  });
