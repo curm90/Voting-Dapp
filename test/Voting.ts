6e9d9957b6eb8761251fcaddec5af0999a96e0ec
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-19 09:20:55 +0100

    test for only owner should create proposals

--- test/Voting.ts
+++ test/Voting.ts
@@ -3,15 +3,16 @@ import { expect } from 'chai';
 
 describe('Voting', function () {
   async function deployContract() {
+    const [owner, addr1] = await ethers.getSigners();
     const Voting = await hre.ethers.getContractFactory('Voting');
-    const voting = await Voting.deploy();
+    const voting = await Voting.deploy(owner.address);
 
-    return voting;
+    return { voting, owner, addr1 };
   }
 
   it('Should create a proposal', async function () {
     const description = 'Proposal 1';
-    const voting = await deployContract();
+    const { voting } = await deployContract();
     await voting.createProposal(description);
 
     const proposals = await voting.getProposals();
@@ -22,7 +23,7 @@ describe('Voting', function () {
 
   it('Should vote for a proposal', async function () {
     const description = 'Proposal 1';
-    const voting = await deployContract();
+    const { voting } = await deployContract();
     await voting.createProposal(description);
 
     await voting.vote(0);
@@ -34,7 +35,7 @@ describe('Voting', function () {
   it('Should return all proposals', async function () {
     const description1 = 'Proposal 1';
     const description2 = 'Proposal 2';
-    const voting = await deployContract();
+    const { voting } = await deployContract();
 
     await voting.createProposal(description1);
     await voting.createProposal(description2);
@@ -43,5 +44,20 @@ describe('Voting', function () {
     expect(proposals.length).to.equal(2);
     expect(proposals[0].description).to.equal(description1);
     expect(proposals[1].description).to.equal(description2);
+  });
+
+  it('Should only allow the owner of contract to create a proposal', async function () {
+    const description1 = 'Proposal 1';
+    const { voting, addr1 } = await deployContract();
+
+    const proposals = await voting.getProposals();
+    expect(proposals.length).to.equal(0);
+
+    try {
+      await voting.connect(addr1).createProposal(description1);
+    } catch (error) {
+      console.log({ message: error.message });
+      expect(error.message).to.include('OwnableUnauthorizedAccount');
+    }
   });
 });
