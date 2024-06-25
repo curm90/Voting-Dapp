13c8d6b38ad467e7e680794042671d4a1d53d49c
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-25 21:00:11 +0100

    cleanup nav bar and only show create proposal button if owner wallet is connected

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useActiveAccount, useReadContract } from 'thirdweb/react';
 import ConnectWalletBtn from '../ConnectWalletBtn/ConnectWalletBtn';
+import { votingContract } from '@/lib/voting';
 
 // only show create proposal btn if the connected account address is the same as the owner of the contract
 // if the button is shown
@@ -10,11 +16,37 @@ import ConnectWalletBtn from '../ConnectWalletBtn/ConnectWalletBtn';
 // on successful creation it should alert the owner the proposal was created and then show a button to link to the proposal page
 // If there was an error alert the user something went wrong
 
-export default async function Navbar() {
+export default function Navbar() {
+  const wallet = useActiveAccount();
+  const address = wallet?.address;
+  const router = useRouter();
+  const [isOwner, setIsOwner] = useState(false);
+
+  const { data: owner } = useReadContract({ contract: votingContract, method: 'owner', params: [] });
+
+  useEffect(() => {
+    if (owner && address) {
+      if (owner.toLowerCase() === address.toLowerCase()) {
+        return setIsOwner(true);
+      }
+
+      setIsOwner(false);
+    }
+  }, [owner, address]);
+
   return (
     <nav className='flex w-full items-center justify-between gap-2 bg-indigo-950 px-12 py-4 text-gray-100'>
       <h4>Voting Dapp</h4>
-      <div>
+      <div className='flex items-center gap-4'>
+        {isOwner ? (
+          <button
+            className='rounded-md bg-indigo-500 px-4 py-2 text-gray-50 duration-150 hover:bg-indigo-400'
+            type='button'
+            onClick={() => router.push('/create-proposal')}
+          >
+            Create proposal
+          </button>
+        ) : null}
         <ConnectWalletBtn />
       </div>
     </nav>
