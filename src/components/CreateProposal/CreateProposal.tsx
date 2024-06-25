3066bff1888576083ebe03cecf8237badf2ccb4e
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-25 21:00:32 +0100

    cleanup create proposal  component to start fresh

--- src/components/CreateProposal/CreateProposal.tsx
+++ src/components/CreateProposal/CreateProposal.tsx
@@ -1,33 +1,13 @@
 'use client';
 
-import { useWallet } from '@/contexts/WalletProvider';
-import { getContract } from '@/lib/voting';
 import { useState } from 'react';
 
 export default function CreateProposal() {
   const [inputVal, setInputVal] = useState('');
-  const { provider, signer } = useWallet();
 
-  console.log({ inputVal });
-
-  async function handleCreateProposal() {
-    if (!provider || !signer) {
-      alert('Please connect your wallet first');
-      return;
-    }
-
-    try {
-      const contract = await getContract();
-      const tx = await contract.connect(signer).createProposal(inputVal);
-      await tx.wait();
-
-      setInputVal('');
-      alert('Proposal created successfully');
-    } catch (error) {
-      console.log({ error });
-      alert('Something went wrong');
-    }
-  }
+  // async function handleCreateProposal() {
+  //   // TODO
+  // }
 
   return (
     <div>
@@ -39,7 +19,7 @@ export default function CreateProposal() {
       />
       <button
         className='rounded-md bg-indigo-500 px-4 py-2 text-gray-50 duration-150 hover:bg-indigo-400'
-        onClick={handleCreateProposal}
+        // onClick={handleCreateProposal}
       >
         Create proposal
       </button>
