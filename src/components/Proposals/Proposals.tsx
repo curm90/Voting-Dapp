c29e3675b77a27d3bec94e7fdce2a37eec533563
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-27 14:25:16 +0100

    render proposals with yes and no vote buttons

--- src/components/Proposals/Proposals.tsx
+++ src/components/Proposals/Proposals.tsx
@@ -14,13 +14,35 @@ export default function Proposals() {
     params: [],
   });
 
-  console.log({ proposals, isLoading, isError });
+  console.log({ isLoading, isError });
 
   if (isLoading) return <span>Loading...</span>;
 
   return (
-    <section className='h-[30rem] w-[20rem] rounded-md border border-gray-600'>
-      <h1>Proposals</h1>
+    <section className='h-minus-header flex items-center justify-center'>
+      <div className='flex flex-col gap-2'>
+        <h1 className='mb-2 text-2xl'>Proposals</h1>
+        {/* @ts-ignore */}
+        {proposals?.map(({ description, voteCount }) => (
+          <div
+            key={description}
+            className='flex justify-between gap-8 rounded-lg border border-violet-600 bg-violet-100 px-4 py-2'
+          >
+            <div className='flex gap-2'>
+              <p>{description}</p>
+              <span>{voteCount}</span>
+            </div>
+
+            <div>
+              <span>Cast vote</span>
+              <div className='flex items-center gap-4'>
+                <button className='rounded-md bg-green-400 px-3 py-1 text-white'>Yes</button>
+                <button className='rounded-md bg-red-400 px-3 py-1 text-white'>No</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
