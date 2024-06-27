4bec7e0b354490a84d44b0beea30cb177c83a4b9
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-27 11:15:52 +0100

    read proposals from contract

--- src/components/Proposals/Proposals.tsx
+++ src/components/Proposals/Proposals.tsx
@@ -1,4 +1,23 @@
+'use client';
+
+import { votingContract } from '@/lib/voting';
+import { useReadContract } from 'thirdweb/react';
+
 export default function Proposals() {
+  const {
+    data: proposals,
+    isLoading,
+    isError,
+  } = useReadContract({
+    contract: votingContract,
+    method: 'getProposals',
+    params: [],
+  });
+
+  console.log({ proposals, isLoading, isError });
+
+  if (isLoading) return <span>Loading...</span>;
+
   return (
     <section className='h-[30rem] w-[20rem] rounded-md border border-gray-600'>
       <h1>Proposals</h1>
