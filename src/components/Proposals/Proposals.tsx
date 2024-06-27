0666873ac15ac5cdc225df416c3e2f4bc963bfcf
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-27 14:56:01 +0100

    implement voting

--- src/components/Proposals/Proposals.tsx
+++ src/components/Proposals/Proposals.tsx
@@ -1,9 +1,13 @@
 'use client';
 
 import { votingContract } from '@/lib/voting';
-import { useReadContract } from 'thirdweb/react';
+import { useState } from 'react';
+import { prepareContractCall } from 'thirdweb';
+import { useReadContract, useSendAndConfirmTransaction } from 'thirdweb/react';
 
 export default function Proposals() {
+  const [loading, setLoading] = useState(false);
+
   const {
     data: proposals,
     isLoading,
@@ -14,7 +18,29 @@ export default function Proposals() {
     params: [],
   });
 
-  console.log({ isLoading, isError });
+  const { mutate: sendTx, isPending, data: txReceipt } = useSendAndConfirmTransaction();
+
+  const handleVote = async (proposalId: number) => {
+    console.log({ proposalId });
+
+    if (typeof proposalId !== 'number') return;
+
+    try {
+      const tx = prepareContractCall({
+        contract: votingContract,
+        method: 'function vote(uint256 proposalId)',
+        params: [BigInt(proposalId)],
+      });
+
+      console.log({ tx });
+
+      sendTx(tx);
+      console.log({ tx });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+  console.log({ txReceipt, isPending });
 
   if (isLoading) return <span>Loading...</span>;
 
@@ -23,22 +49,23 @@ export default function Proposals() {
       <div className='flex flex-col gap-2'>
         <h1 className='mb-2 text-2xl'>Proposals</h1>
         {/* @ts-ignore */}
-        {proposals?.map(({ description, voteCount }) => (
+        {proposals?.map(({ description, voteCount }, index) => (
           <div
             key={description}
-            className='flex justify-between gap-8 rounded-lg border border-violet-600 bg-violet-100 px-4 py-2'
+            className='flex items-center justify-between gap-8 rounded-lg border border-violet-600 bg-violet-100 px-4 py-2'
           >
             <div className='flex gap-2'>
               <p>{description}</p>
               <span>{voteCount}</span>
             </div>
 
-            <div>
-              <span>Cast vote</span>
-              <div className='flex items-center gap-4'>
-                <button className='rounded-md bg-green-400 px-3 py-1 text-white'>Yes</button>
-                <button className='rounded-md bg-red-400 px-3 py-1 text-white'>No</button>
-              </div>
+            <div className='flex items-center gap-4'>
+              <button
+                className='rounded-md bg-green-400 px-3 py-1 text-white'
+                onClick={() => handleVote(index)}
+              >
+                Vote
+              </button>
             </div>
           </div>
         ))}
